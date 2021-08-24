c0bb979a4c2ad01e9b39c405c3482931fbf564f8
Ryanmunnikhuis <rmunnikhuis@bren.ucsb.edu>
2021-08-24 09:17:54 -0700

    Added code for favorite dessert.

---
+++ desserts_match_myinitials.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+library(here)
+webpage <- read_html("https://www.eatthis.com/iconic-desserts-united-states/")
+
+dessert_elements<- html_elements(webpage, "h2")
+
+dessert_listing <- dessert_elements %>% html_text2() %>%
+  as_tibble() %>% rename(dessert = value) %>%  head(.,-3) %>%
+  rowid_to_column("rank") %>%
+  write_csv("data/iconic_desserts.csv")
+
+print(str_detect(iconic_desserts$dessert, "brownies"))
